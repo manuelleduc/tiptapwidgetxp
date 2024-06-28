a9668866209015f70b423f85c009e8223cbe9551
Manuel Leduc <manuel.leduc@xwiki.com>
2024-06-28 15:50:47 +0200

    First version: reactive UI element.

---
+++ src/components/ExtensionRootTemplate.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import {NodeViewWrapper, nodeViewProps} from "@tiptap/vue-3";
+
+const props = defineProps(nodeViewProps);
+
+function increment() {
+  props.updateAttributes({
+    count: props.node.attrs.count + 1
+  })
+}
+</script>
+
+<template>
+  <node-view-wrapper>
+    <div>Hello World {{ node.attrs.count }}</div>
+    <div>
+      <button @click="increment">increment</button>
+    </div>
+  </node-view-wrapper>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/RootComponent.vue
@@ -0,0 +1,36 @@
+<script setup>
+import {Editor, EditorContent} from "@tiptap/vue-3";
+import {StarterKit} from "@tiptap/starter-kit";
+import MyExtension from "../extension";
+import VueJsonPretty from 'vue-json-pretty'
+import 'vue-json-pretty/lib/styles.css';
+import VCodeBlock from '@wdns/vue-code-block';
+import sb from 'simply-beautiful';
+const beautify = sb.html;
+
+const content = "<p>\n" +
+    "          This is still the text editor you’re used to, but enriched with node views.\n" +
+    "        </p>\n" +
+    "        <vue-component></vue-component>\n" +
+    "        <p>\n" +
+    "          Did you see that? That’s a Vue component. We are really living in the future.\n" +
+    "        </p>"
+
+const editor = new Editor({
+  extensions: [StarterKit, MyExtension],
+  content
+})
+</script>
+
+<template>
+  Editor:
+  <editor-content :editor="editor"/>
+  Content (json):
+  <vue-json-pretty :data="editor.getJSON()"></vue-json-pretty>
+  Content (html):
+  <v-code-block :code="beautify(editor.getHTML())" highlightjs lang="html"></v-code-block>
+</template>
+
+<style scoped>
+
+</style>
